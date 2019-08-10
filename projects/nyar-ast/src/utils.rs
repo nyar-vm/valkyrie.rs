2ee99e95c275ab8d36db4b48578e0731e87f2892
Aster <galaster@foxmail.com>
2019-08-10 09:36:14 +0800

    🌿 Support AST transform

---
+++ projects/nyar-ast/src/utils.rs
@@ -0,0 +1,2 @@
+pub mod build_ast;
+pub mod build_import;
---
+++ projects/nyar-ast/src/utils/build_ast.rs
@@ -0,0 +1,19 @@
+use crate::ast::AST;
+
+#[allow(unused_variables)]
+impl AST {
+    pub fn parse_string(self) -> AST {
+        return match self {
+            AST::StringLiteral { handler, data } => AST::String("".to_string()),
+            AST::String(s) => AST::String(s),
+            _ => AST::String("".to_string()),
+        };
+    }
+    pub fn save(self, path: &str) {
+        let json = serde_json::to_string(&self).unwrap();
+        println!("{}", json);
+    }
+    pub fn load(path: &str) -> AST {
+        unimplemented!()
+    }
+}
