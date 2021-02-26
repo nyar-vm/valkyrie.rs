c390b1751edc9e7031ca0518c6100ec00b97dd9b
Aster <aster@vers.site>
2021-02-26 19:46:14 +0000

    🧪 Wrap to native types

--- projects/valkyrie-types/src/builtin/number/mod.rs
+++ projects/valkyrie-types/src/builtin/number/mod.rs
@@ -1,8 +1,9 @@
+use crate::values::ValkyrieValue;
 use std::mem::MaybeUninit;
 use wasmtime::{
     component::{
         ComponentType, Lift, Lower,
-        __internal::{CanonicalAbiInfo, InstanceType, InterfaceType, LiftContext, LowerContext},
+        __internal::{anyhow::bail, CanonicalAbiInfo, InstanceType, InterfaceType, LiftContext, LowerContext},
     },
     ValRaw,
 };
@@ -20,82 +21,27 @@ pub enum ValkyrieNumber {
     Float64(f64),
 }
 
-unsafe impl ComponentType for ValkyrieNumber {
-    type Lower = ValRaw;
-    const ABI: CanonicalAbiInfo = Default::default();
-
-    fn typecheck(ty: &InterfaceType, types: &InstanceType<'_>) -> wasmtime::Result<()> {
-        todo!()
+impl From<u8> for ValkyrieNumber {
+    #[inline]
+    fn from(value: u8) -> Self {
+        Self::Unsigned8(value)
     }
 }
 
-unsafe impl Lift for ValkyrieNumber {
-    fn lift(cx: &mut LiftContext<'_>, ty: InterfaceType, src: &Self::Lower) -> wasmtime::Result<Self> {
-        match ty {
-            InterfaceType::Bool => {}
-            InterfaceType::S8 => {}
-            InterfaceType::U8 => {}
-            InterfaceType::S16 => {}
-            InterfaceType::U16 => {}
-            InterfaceType::S32 => {}
-            InterfaceType::U32 => {}
-            InterfaceType::S64 => {}
-            InterfaceType::U64 => {}
-            InterfaceType::Float32 => {}
-            InterfaceType::Float64 => {}
-            InterfaceType::Char => {}
-            InterfaceType::String => {}
-            InterfaceType::Record(_) => {}
-            InterfaceType::Variant(_) => {}
-            InterfaceType::List(_) => {}
-            InterfaceType::Tuple(_) => {}
-            InterfaceType::Flags(_) => {}
-            InterfaceType::Enum(_) => {}
-            InterfaceType::Option(_) => {}
-            InterfaceType::Result(_) => {}
-            InterfaceType::Own(_) => {}
-            InterfaceType::Borrow(_) => {}
-        }
-    }
-
-    fn load(cx: &mut LiftContext<'_>, ty: InterfaceType, bytes: &[u8]) -> wasmtime::Result<Self> {
-        todo!()
+impl From<u8> for ValkyrieValue {
+    #[inline]
+    fn from(value: u8) -> Self {
+        Self::Number(ValkyrieNumber::from(value))
     }
 }
 
-unsafe impl Lower for ValkyrieNumber {
-    fn lower<T>(
-        &self,
-        cx: &mut LowerContext<'_, T>,
-        ty: InterfaceType,
-        dst: &mut MaybeUninit<Self::Lower>,
-    ) -> wasmtime::Result<()> {
-        match self {
-            Self::Unsigned8(v) => v.lower(cx, ty, dst),
-            Self::Unsigned16(v) => v.lower(cx, ty, dst),
-            Self::Unsigned32(v) => v.lower(cx, ty, dst),
-            Self::Unsigned64(v) => v.lower(cx, ty, dst),
-            Self::Integer8(v) => v.lower(cx, ty, dst),
-            Self::Integer16(v) => v.lower(cx, ty, dst),
-            Self::Integer32(v) => v.lower(cx, ty, dst),
-            Self::Integer64(v) => v.lower(cx, ty, dst),
-            Self::Float32(v) => v.lower(cx, ty, dst),
-            Self::Float64(v) => v.lower(cx, ty, dst),
-        }
-    }
+macro_rules! map_value {
 
-    fn store<T>(&self, cx: &mut LowerContext<'_, T>, ty: InterfaceType, offset: usize) -> wasmtime::Result<()> {
-        match self {
-            Self::Unsigned8(v) => v.store(cx, ty, offset),
-            Self::Unsigned16(v) => v.store(cx, ty, offset),
-            Self::Unsigned32(v) => v.store(cx, ty, offset),
-            Self::Unsigned64(v) => v.store(cx, ty, offset),
-            Self::Integer8(v) => v.store(cx, ty, offset),
-            Self::Integer16(v) => v.store(cx, ty, offset),
-            Self::Integer32(v) => v.store(cx, ty, offset),
-            Self::Integer64(v) => v.store(cx, ty, offset),
-            Self::Float32(v) => v.store(cx, ty, offset),
-            Self::Float64(v) => v.store(cx, ty, offset),
-        }
-    }
+}
+
+
+map_value! {
+    u8 => Unsigned8,
+    u16 => Unsigned16,
+
 }
--- projects/valkyrie-types/src/values/mod.rs
+++ projects/valkyrie-types/src/values/mod.rs
@@ -5,7 +5,7 @@ use crate::ValkyrieNumber;
 use std::mem::MaybeUninit;
 use wasmtime::{
     component::{
-        ComponentNamedList, ComponentType, Lift, Lower,
+        ComponentNamedList, ComponentType, Lift, Lower, Val,
         __internal::{CanonicalAbiInfo, InstanceType, InterfaceType, LiftContext, LowerContext},
     },
     ValRaw,
@@ -24,6 +24,60 @@ pub enum ValkyrieValue {
     /// Native boolean type, 8bit
     Boolean(bool),
     Number(ValkyrieNumber),
+    Result(Result<ValkyrieValue, ValkyrieValue>)
+}
+
+
+
+impl From<Val> for ValkyrieValue {
+    fn from(value: Val) -> Self {
+        match value {
+            Val::Bool(v) => v.into(),
+            Val::S8(v) => v.into(),
+            Val::U8(v) => v.into(),
+            Val::S16(v) => v.into(),
+            Val::U16(v) => v.into(),
+            Val::S32(v) => v.into(),
+            Val::U32(v) => v.into(),
+            Val::S64(v) => v.into(),
+            Val::U64(v) => v.into(),
+            Val::Float32(v) => v.into(),
+            Val::Float64(v) => v.into(),
+            Val::Char(_) => {
+                todo!()
+            }
+            Val::String(_) => {
+                todo!()
+            }
+            Val::List(_) => {
+                todo!()
+            }
+            Val::Record(_) => {
+                todo!()
+            }
+            Val::Tuple(_) => {
+                todo!()
+            }
+            Val::Variant(_) => {
+                todo!()
+            }
+            Val::Enum(_) => {
+                todo!()
+            }
+            Val::Option(v) => {
+                Maybe::
+            }
+            Val::Result(_) => {
+                todo!()
+            }
+            Val::Flags(_) => {
+                todo!()
+            }
+            Val::Resource(_) => {
+                todo!()
+            }
+        }
+    }
 }
 
 unsafe impl ComponentNamedList for ValkyrieValue {}
@@ -39,11 +93,103 @@ unsafe impl ComponentType for ValkyrieValue {
 
 unsafe impl Lift for ValkyrieValue {
     fn lift(cx: &mut LiftContext<'_>, ty: InterfaceType, src: &Self::Lower) -> wasmtime::Result<Self> {
-        todo!()
+        match ty {
+            InterfaceType::Bool => bool::lift(cx, ty, src).map(Into::into),
+            InterfaceType::S8 => bool::lift(cx, ty, src).map(Into::into),
+            InterfaceType::U8 => bool::lift(cx, ty, src).map(Into::into),
+            InterfaceType::S16 => bool::lift(cx, ty, src).map(Into::into),
+            InterfaceType::U16 => bool::lift(cx, ty, src).map(Into::into),
+            InterfaceType::S32 => bool::lift(cx, ty, src).map(Into::into),
+            InterfaceType::U32 => bool::lift(cx, ty, src).map(Into::into),
+            InterfaceType::S64 => bool::lift(cx, ty, src).map(Into::into),
+            InterfaceType::U64 => bool::lift(cx, ty, src).map(Into::into),
+            InterfaceType::Float32 => bool::lift(cx, ty, src).map(Into::into),
+            InterfaceType::Float64 => bool::lift(cx, ty, src).map(Into::into),
+            InterfaceType::Char => char::lift(),
+            InterfaceType::String => String::lift(cx, ty, src),
+            InterfaceType::Record(_) => {
+                todo!()
+            }
+            InterfaceType::Variant(_) => {
+                todo!()
+            }
+            InterfaceType::List(_) => {
+                todo!()
+            }
+            InterfaceType::Tuple(_) => {
+                todo!()
+            }
+            InterfaceType::Flags(_) => {
+                todo!()
+            }
+            InterfaceType::Enum(_) => {
+                todo!()
+            }
+            InterfaceType::Option(_) => {
+                todo!()
+            }
+            InterfaceType::Result(_) => {
+                todo!()
+            }
+            InterfaceType::Own(_) => {
+                todo!()
+            }
+            InterfaceType::Borrow(_) => {
+                todo!()
+            }
+        }
     }
 
     fn load(cx: &mut LiftContext<'_>, ty: InterfaceType, bytes: &[u8]) -> wasmtime::Result<Self> {
-        todo!()
+        match ty {
+            InterfaceType::Bool => bool::load(cx, ty, bytes).map(Into::into),
+            InterfaceType::S8 => bool::load(cx, ty, bytes).map(Into::into),
+            InterfaceType::U8 => bool::load(cx, ty, bytes).map(Into::into),
+            InterfaceType::S16 => bool::load(cx, ty, bytes).map(Into::into),
+            InterfaceType::U16 => bool::load(cx, ty, bytes).map(Into::into),
+            InterfaceType::S32 => bool::load(cx, ty, bytes).map(Into::into),
+            InterfaceType::U32 => bool::load(cx, ty, bytes).map(Into::into),
+            InterfaceType::S64 => bool::load(cx, ty, bytes).map(Into::into),
+            InterfaceType::U64 => bool::load(cx, ty, bytes).map(Into::into),
+            InterfaceType::Float32 => bool::load(cx, ty, bytes).map(Into::into),
+            InterfaceType::Float64 => bool::load(cx, ty, bytes).map(Into::into),
+            InterfaceType::Char => {
+                todo!()
+            }
+            InterfaceType::String => {
+                todo!()
+            }
+            InterfaceType::Record(_) => {
+                todo!()
+            }
+            InterfaceType::Variant(_) => {
+                todo!()
+            }
+            InterfaceType::List(_) => {
+                todo!()
+            }
+            InterfaceType::Tuple(_) => {
+                todo!()
+            }
+            InterfaceType::Flags(_) => {
+                todo!()
+            }
+            InterfaceType::Enum(_) => {
+                todo!()
+            }
+            InterfaceType::Option(_) => {
+                todo!()
+            }
+            InterfaceType::Result(_) => {
+                todo!()
+            }
+            InterfaceType::Own(_) => {
+                todo!()
+            }
+            InterfaceType::Borrow(_) => {
+                todo!()
+            }
+        }
     }
 }
 
@@ -55,14 +201,45 @@ unsafe impl Lower for ValkyrieValue {
         dst: &mut MaybeUninit<Self::Lower>,
     ) -> wasmtime::Result<()> {
         match self {
-            ValkyrieValue::Nothing => {}
-            ValkyrieValue::Unit => {}
+            ValkyrieValue::Nothing => {
+                todo!()
+            }
+            ValkyrieValue::Unit => vec![].lower(cx, ty, dst),
             ValkyrieValue::Boolean(v) => v.lower(cx, ty, dst),
-            ValkyrieValue::Number(v) => {}
+            ValkyrieValue::Number(v) => match v {
+                ValkyrieNumber::Unsigned8(v) => v.lower(cx, ty, dst),
+                ValkyrieNumber::Unsigned16(v) => v.lower(cx, ty, dst),
+                ValkyrieNumber::Unsigned32(v) => v.lower(cx, ty, dst),
+                ValkyrieNumber::Unsigned64(v) => v.lower(cx, ty, dst),
+                ValkyrieNumber::Integer8(v) => v.lower(cx, ty, dst),
+                ValkyrieNumber::Integer16(v) => v.lower(cx, ty, dst),
+                ValkyrieNumber::Integer32(v) => v.lower(cx, ty, dst),
+                ValkyrieNumber::Integer64(v) => v.lower(cx, ty, dst),
+                ValkyrieNumber::Float32(v) => v.lower(cx, ty, dst),
+                ValkyrieNumber::Float64(v) => v.lower(cx, ty, dst),
+            },
         }
     }
 
     fn store<T>(&self, cx: &mut LowerContext<'_, T>, ty: InterfaceType, offset: usize) -> wasmtime::Result<()> {
-        todo!()
+        match self {
+            ValkyrieValue::Nothing => {
+                todo!()
+            }
+            ValkyrieValue::Unit => ().store(cx, ty, offset),
+            ValkyrieValue::Boolean(v) => v.store(cx, ty, offset),
+            ValkyrieValue::Number(v) => match v {
+                ValkyrieNumber::Unsigned8(v) => v.v(cx, ty, offset),
+                ValkyrieNumber::Unsigned16(v) => v.store(cx, ty, offset),
+                ValkyrieNumber::Unsigned32(v) => v.store(cx, ty, offset),
+                ValkyrieNumber::Unsigned64(v) => v.store(cx, ty, offset),
+                ValkyrieNumber::Integer8(v) => v.store(cx, ty, offset),
+                ValkyrieNumber::Integer16(v) => v.store(cx, ty, offset),
+                ValkyrieNumber::Integer32(v) => v.store(cx, ty, offset),
+                ValkyrieNumber::Integer64(v) => v.store(cx, ty, offset),
+                ValkyrieNumber::Float32(v) => v.store(cx, ty, offset),
+                ValkyrieNumber::Float64(v) => v.store(cx, ty, offset),
+            },
+        }
     }
 }
